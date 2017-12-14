019e64f05225a6d1f3807019c96118e02302204c
Nykyta1995 <phenix_2014@bk.ru>
2017-12-14 22:00:05 +0200

    Implementation

--- WPFExplorer/AboutBox1.Designer.cs
+++ WPFExplorer/AboutBox1.Designer.cs
@@ -86,7 +86,7 @@
             this.labelProductName.Name = "labelProductName";
             this.labelProductName.Size = new System.Drawing.Size(271, 17);
             this.labelProductName.TabIndex = 19;
-            this.labelProductName.Text = "Название продукта";
+            this.labelProductName.Text = "MyExplorer";
             this.labelProductName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelVersion
